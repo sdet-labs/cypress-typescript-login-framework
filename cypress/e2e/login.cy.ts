bec60442f89358b689ccbfe0b4e4e2c324fa4ea7
Sumithra-Suresh <jasisumi@gmail.com>
2026-01-09 20:04:53 +0000

    Added Custom  Cypress command for login

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,16 +1,15 @@
 import LoginPage from '../pages/LoginPage';
 
 describe('Login Functionality', () => {
-
   const loginPage = new LoginPage();
 
-  beforeEach(() => {
+  beforeEach(function () {
     cy.fixture('users').as('users');
     loginPage.visit();
   });
 
   it('should login successfully with valid credentials', function () {
-    loginPage.login(
+    cy.login(
       this.users.validUser.username,
       this.users.validUser.password
     );
@@ -19,7 +18,7 @@ describe('Login Functionality', () => {
   });
 
   it('should fail login with invalid credentials', function () {
-    loginPage.login(
+    cy.login(
       this.users.invalidUser.username,
       this.users.invalidUser.password
     );
@@ -29,30 +28,26 @@ describe('Login Functionality', () => {
   });
 
   it('should not allow login for locked out user', function () {
-    loginPage.login(
+    cy.login(
       this.users.lockedUser.username,
       this.users.lockedUser.password
     );
-  
+
     loginPage.errorMessage()
       .should('contain.text', 'Sorry, this user has been locked out');
   });
 
-  it('should show validation error when username and password are empty', () => {
-    cy.get('#login-button').click();
+  it('should show validation error when username and password are empty', function () {
+    cy.login('', '');
 
-    cy.get('[data-test="error"]')
-      .should('be.visible')
-      .and('contain.text', 'Username is required');
+    loginPage.errorMessage()
+      .should('contain.text', 'Username is required');
   });
 
   it('should show validation error when password is empty', function () {
-    cy.get('#user-name').type(this.users.validUser.username);
-    cy.get('#login-button').click();
-  
-    cy.get('[data-test="error"]')
+    cy.login(this.users.validUser.username, '');
+
+    loginPage.errorMessage()
       .should('contain.text', 'Password is required');
   });
-
-
 });
